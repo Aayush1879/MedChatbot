099ed1d223ab4c6283aaa08945b0f1925fc9d473
Aayush Dangi <aayushdangi79@gmal.com>
2026-04-12 12:35:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_lung_chatbot",
+    version="0.1.0",
+    author="Aayush Dangi",
+    author_email="aayushdangi79@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
